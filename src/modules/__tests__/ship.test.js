5e203ef56bca710f5edccfaa94ef4478328926a2
CameronTheDeveloper <Cameronposey@outlook.com>
2023-09-04 19:55:38 -0700

    Add ship hit, isSunk methods

--- src/modules/__tests__/ship.test.js
+++ src/modules/__tests__/ship.test.js
@@ -1,8 +1,21 @@
 import { ship } from '../ship';
 
 describe('Ship object', () => {
-    const newShip = ship(5);
-    it('Has a length', () => {
-        expect(newShip.length).toBe(5);
+    const ship1 = ship(3);
+
+    it('takes hits', () => {
+        ship1.hit();
+        ship1.hit();
+        expect(ship1.hits).toBe(2);
+    });
+
+    it('hasn\'t sunk with hits less than length', () => {
+        expect(ship1.sunk).toBe(false);
+    });
+
+    it('has sunk when hits = length', () => {
+        ship1.hit();
+        expect(ship1.sunk).toBe(true);
     });
 });
+
--- src/modules/ship.js
+++ src/modules/ship.js
@@ -1,8 +1,18 @@
 const ship = (length) => {
     return {
         length: length,
-        health: length,
+        hits: 0,
         sunk: false,
+
+        hit() {
+            this.hits++;
+            this.isSunk();
+        },
+        isSunk() {
+            if (this.hits >= this.length) {
+                this.sunk = true;
+            }
+        }
     };
 };
 
