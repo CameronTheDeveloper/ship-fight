f792eeb6480e32c6d51d01b8dba6b8aedae871eb
CameronTheDeveloper <Cameronposey@outlook.com>
2023-09-10 00:21:14 -0700

    Add board object positions and adjacent positions

--- src/modules/board.js
+++ src/modules/board.js
@@ -7,6 +7,7 @@ const boardPos = (x, y) => {
 };
 
 const board = () => {
+
     return {
         pos: new Map(),
         length: 0,
@@ -22,59 +23,39 @@ const board = () => {
             this.pos.get(position).push(adjPosition);
         },
 
-        _getLengthAr(lengthInput) {
-            let lengthAr = [];
-            for (let i = 0; i < lengthInput; i++) {
-                this.length++;
-                lengthAr.push(i);
-            }
-            return lengthAr;
-        },
+        _connectPositions() {
+            const adjPosDistances = [[0, 1], [1, 1], [1, 0], [1, -1],
+            [0, -1], [-1, -1], [-1, 0], [-1, 1]];
+            let posKeys = this.pos.keys();
 
-        _getWidthAr(widthInput) {
-            let widthAr = [];
-            for (let i = 0; i < widthInput; i++) {
-                this.width++;
-                widthAr.push(i);
-            }
-            return widthAr;
-        },
-
-        setSize(lengthInput, widthInput) {
-            const lengthAr = this._getLengthAr(lengthInput);
-            const widthAr = this._getWidthAr(widthInput);
-
-            for (let i = 0; i < lengthAr.length; i++) {
-                for (let j = 0; j < widthAr.length; j++) {
-                    this.setPosition([i, j]);
+            for (let position of posKeys) {
+                for (let adjPosDistance of adjPosDistances) {
+                    let xPos = position.x + adjPosDistance[0];
+                    let yPos = position.y + adjPosDistance[1];
+                    if (xPos >= 1 && yPos >= 1 && xPos <= this.width && yPos <= this.length) {
+                        let adjPosition = [xPos, yPos];
+                        this.setAdjacentPosition(position, adjPosition);
+                    }
                 }
             }
-
         },
 
-        addShip(ship) {
-            shipCount++;
+        setSize(widthInput, lengthInput) {
+            this.width = widthInput;
+            this.length = lengthInput;
+
+            for (let i = 1; i <= this.width; i++) {
+                for (let j = 1; j <= this.length; j++) {
+                    this.setPosition(boardPos(i, j));
+                }
+            }
+            this._connectPositions();
         },
+
+        // addShip(ship) {
+        //     shipCount++;
+        // },
     };
 };
-
-// const connectPositions = (board, positionAr) => {
-//     let widthAr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-//     let heightAr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-//     for (let position of positionAr) {
-//         for (let i = 0; i < widthAr.length; i++) {
-//             let xPos = position.x + widthAr[i];
-//             let yPos = position.y + heightAr[i];
-//             if (xPos >= 1 && yPos >= 1 && xPos <= 10 && yPos <= 10) {
-//                 let adjPosition = [xPos, yPos];
-//                 board.addAdjacentPosition(position, adjPosition);
-//             }
-//         }
-//     }
-// };
-
-// const addBoardPositions = (board) => {
-
-// };
 
 export { board };
