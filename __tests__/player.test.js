b183c0cfb4b7fb798f63c6202c06ff56e591f51e
CameronTheDeveloper <Cameronposey@outlook.com>
2023-09-21 22:52:28 -0700

    Add player.test.js test cases

--- __tests__/player.test.js
+++ __tests__/player.test.js
@@ -1,23 +1,37 @@
 import { board } from "../src/modules/board";
+import { ship } from "../src/modules/ship";
 
-xdescribe('Player object', () => {
+describe('Player object', () => {
     let player1;
     let player2;
+    let ship1;
 
     beforeEach(() => {
         player1 = player();
         player2 = player();
-
         player1.playerBoard = board();
         player2.playerBoard = board();
+        ship1 = ship(1);
 
         player1.playerBoard.setSize(10, 10);
         player2.playerBoard.setSize(10, 10);
+        player2.playerBoard.placeShip(ship1, [2, 2]);
     });
 
-    xit('should be able to attack enemy game board', () => {
+    xit('should be able to attack enemy game board position', () => {
         player1.attackPos([5, 4]);
         expect(player2.playerBoard.attackedPositions[[5, 4]]).toEqual(true);
+    });
+
+    xit('should prevent attacks after a miss', () => {
+        player1.attackPos([5, 4]);
+        expect(player1.attackPos[7, 2]).toBe(null);
+    });
+
+    xit('should attack again after a hit', () => {
+        player1.attackPos([2, 2]);
+        player1.attackPos([7, 2]);
+        expect(player2.playerBoard.attackedPositions[[7, 2]]).toEqual(true);
     });
 });
 
@@ -25,8 +39,6 @@ xdescribe('Player object', () => {
 xdescribe('Player AI', () => {
     let player1;
     let player2;
-
-
 
     beforeEach(() => {
         player1 = player();
