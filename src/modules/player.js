fa9f9d8702824523d9af3cbee47bbe14faadcd9e
CameronTheDeveloper <Cameronposey@outlook.com>
2023-09-25 01:07:26 -0700

    Add player object random attack functions

--- src/modules/player.js
+++ src/modules/player.js
@@ -23,6 +23,20 @@ const player = (name = 'player') => {
             }
             return attackedPos;
         },
+
+        _getRandomAttackPos() {
+            const enemyBoardLength = this.enemy.playerBoard.availableAttacks.length;
+            const randomIndex = Math.floor(Math.random(0, enemyBoardLength - 1));
+
+            return this.enemy.playerBoard.availableAttacks[randomIndex];
+        },
+
+        attackRandomPos() {
+            const randomPos = this._getRandomAttackPos();
+            const attackedPos = this.attackPos(JSON.parse(randomPos));
+
+            return attackedPos;
+        },
     };
 };
 
