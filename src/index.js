77b65e4f0b76a77d9481f3322f3a7c3d0e148c71
CameronTheDeveloper <Cameronposey@outlook.com>
2023-09-27 03:00:26 -0700

    Add index.js renderGame function, create boards

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import './styles/index.css';
 import './styles/board.css';
+import { createPlayerBoard } from './modules/game-manager';
 
+const renderGame = () => {
+    const leftBoardDiv = document.querySelector('#board-left');
+    const rightBoardDiv = document.querySelector('#board-right');
+
+    const leftBoard = createPlayerBoard(4, 4, leftBoardDiv);
+    const rightBoard = createPlayerBoard(4, 4, rightBoardDiv);
+
+};
+
+renderGame();
