af3298111e9a308ee6892f245c88708fbb2f1121
CameronTheDeveloper <Cameronposey@outlook.com>
2023-09-28 13:11:40 -0700

    Add board pos mouse-hover events

--- src/modules/user-input.js
+++ src/modules/user-input.js
@@ -4,8 +4,18 @@ const addPosClickEvent = (pos) => {
     });
 };
 
+const addPosHoverEvents = (pos) => {
+    pos.addEventListener('mouseover', () => {
+        pos.style.backgroundColor = 'rgba(0, 0, 0, .7)';
+    });
+    pos.addEventListener('mouseleave', () => {
+        pos.style.backgroundColor = '';
+    });
+};
+
 const addPosMouseEvents = (pos) => {
     addPosClickEvent(pos);
+    addPosHoverEvents(pos);
 };
 
 export { addPosMouseEvents };
