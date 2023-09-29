0f86cfe52a3dd43955d08d892e3205828fe322cb
CameronTheDeveloper <Cameronposey@outlook.com>
2023-09-29 01:10:44 -0700

    Add game-manager.js createPlayer function

--- src/modules/game-manager.js
+++ src/modules/game-manager.js
@@ -1,4 +1,5 @@
 import { board } from "./board";
+import { player } from "./player";
 import { displayBoardPositions } from "./page-dom";
 
 const createPlayerBoard = (width, length, boardDiv) => {
@@ -10,12 +11,22 @@ const createPlayerBoard = (width, length, boardDiv) => {
     return playerBoard;
 };
 
+const createPlayer = (board, playerName) => {
+    const newPlayer = player(playerName);
+
+    newPlayer.playerBoard = board;
+    return newPlayer;
+};
+
 const initializeGame = () => {
     const leftBoardDiv = document.querySelector('#board-left');
     const rightBoardDiv = document.querySelector('#board-right');
 
     const leftBoard = createPlayerBoard(4, 4, leftBoardDiv);
     const rightBoard = createPlayerBoard(4, 4, rightBoardDiv);
+
+    const leftPlayer = createPlayer(leftBoard, 'Player 1');
+    const rightPlayer = createPlayer(rightBoard, 'Player 2');
 };
 
 export { initializeGame };
