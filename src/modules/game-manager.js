f288d18f286236c658003dd4d9c1ff9de2cd287a
CameronTheDeveloper <Cameronposey@outlook.com>
2023-09-29 04:24:26 -0700

    Remove game-manager.js createPlayer function

--- src/modules/game-manager.js
+++ src/modules/game-manager.js
@@ -11,22 +11,15 @@ const createPlayerBoard = (width, length, boardDiv) => {
     return playerBoard;
 };
 
-const createPlayer = (board, playerName) => {
-    const newPlayer = player(playerName);
-
-    newPlayer.playerBoard = board;
-    return newPlayer;
-};
-
 const initializeGame = () => {
     const leftBoardDiv = document.querySelector('#board-left');
     const rightBoardDiv = document.querySelector('#board-right');
 
-    const leftBoard = createPlayerBoard(4, 4, leftBoardDiv);
-    const rightBoard = createPlayerBoard(4, 4, rightBoardDiv);
+    const leftPlayer = player('Player 1');
+    const rightPlayer = player('Player 2');
 
-    const leftPlayer = createPlayer(leftBoard, 'Player 1');
-    const rightPlayer = createPlayer(rightBoard, 'Player 2');
+    leftPlayer.playerBoard = createPlayerBoard(4, 4, leftBoardDiv);
+    rightPlayer.playerBoard = createPlayerBoard(4, 4, rightBoardDiv);
 };
 
 export { initializeGame };
