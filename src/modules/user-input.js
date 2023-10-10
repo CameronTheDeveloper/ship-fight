e15bf02f1d2c6f06256c22b68419160df00ebed9
CameronTheDeveloper <Cameronposey@outlook.com>
2023-10-09 21:39:39 -0700

    Update board property names for add attack loops

--- src/modules/user-input.js
+++ src/modules/user-input.js
@@ -6,14 +6,14 @@ const addPosClickAttack = (posDiv, player, pos) => {
     });
 };
 
+
 const addBoardAttackListeners = (player) => {
     const board = player.playerBoard;
     const boardSideID = board.boardSide;
     let pos = [];
     let posDiv = null;
-
-    for (let y = board.length; y >= 1; y--) {
-        for (let x = 1; x <= board.width; x++) {
+    for (let y = board.boardLength; y >= 1; y--) {
+        for (let x = 1; x <= board.boardWidth; x++) {
             pos = `${x}_${y}`;
             posDiv = document.querySelector(`#${boardSideID}-${pos}`);
             addPosClickAttack(posDiv, player, pos);
