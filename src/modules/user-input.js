4f857c95c7e51bf60810518c9d4c5b896e423e51
CameronTheDeveloper <Cameronposey@outlook.com>
2023-10-09 23:44:55 -0700

    Add user-input.js placeShip click events

--- src/modules/user-input.js
+++ src/modules/user-input.js
@@ -1,11 +1,11 @@
 import { attackPlayerBoard } from "./page-dom";
+import { placeShipDOM } from "./page-dom";
 
 const addPosClickAttack = (posDiv, player, pos) => {
     posDiv.addEventListener('click', () => {
         attackPlayerBoard(posDiv, player, pos);
     });
 };
-
 
 const addBoardAttackListeners = (player) => {
     const board = player.playerBoard;
@@ -21,4 +21,27 @@ const addBoardAttackListeners = (player) => {
     }
 };
 
-export { addBoardAttackListeners };
+const addPosClickPlaceShip = (posDiv, player, pos) => {
+    const board = player.playerBoard;
+
+    posDiv.addEventListener('click', () => {
+        player.activeShip.cords = board.placeShip(player.activeShip, pos);
+        placeShipDOM(player.activeShip.cords, board.boardSide);
+    });
+};
+
+const addBoardPlaceShipListeners = (player) => {
+    const board = player.playerBoard;
+    const boardSideID = board.boardSide;
+    let pos = [];
+    let posDiv = null;
+    for (let y = board.boardLength; y >= 1; y--) {
+        for (let x = 1; x <= board.boardWidth; x++) {
+            pos = `${x}_${y}`;
+            posDiv = document.querySelector(`#${boardSideID}-${pos}`);
+            addPosClickPlaceShip(posDiv, player, pos);
+        }
+    }
+};
+
+export { addBoardAttackListeners, addBoardPlaceShipListeners };
