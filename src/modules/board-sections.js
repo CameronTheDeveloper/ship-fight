532c69581224c0f23b082ed6d0bc70f89b6c62e9
CameronTheDeveloper <Cameronposey@outlook.com>
2023-10-12 12:52:21 -0700

    Add board-sections.js file, boardSection object

---
+++ src/modules/board-sections.js
@@ -0,0 +1,10 @@
+const boardSection = (minX, maxX, minY, maxY) => {
+    return {
+        minX: minX,
+        maxX: maxX,
+        minY: minY,
+        maxY: maxY,
+    };
+};
+
+export { boardSection };
