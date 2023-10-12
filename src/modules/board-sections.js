0cc0c920e195a83596fc43f4a5a13c70eb401e28
CameronTheDeveloper <Cameronposey@outlook.com>
2023-10-12 14:42:04 -0700

    Add boardSection createHorizontalSection

--- src/modules/board-sections.js
+++ src/modules/board-sections.js
@@ -1,10 +1,47 @@
-const boardSection = (minX, maxX, minY, maxY) => {
+const boardSection = () => {
     return {
-        minX: minX,
-        maxX: maxX,
-        minY: minY,
-        maxY: maxY,
+        minX: null,
+        maxX: null,
+        minY: null,
+        maxY: null,
+
+        _setMinX(newMinX) {
+            this.minX = newMinX;
+            if (this.minX < 1) {
+                this.minX = 1;
+            }
+        },
+
+        _setMinY(newMinY) {
+            this.minY = newMinY;
+            if (this.minY < 1) {
+                this.minY = 1;
+            }
+        },
+
+        _setMaxX(space, board) {
+            this.maxX = this.minX + space;
+            if (this.maxX > board.width) {
+                this.maxX = board.width;
+            }
+        },
+
+        _setMaxY(space, board) {
+            this.maxY = this.minY + space;
+            if (this.maxY > board.height) {
+                this.maxY = boardHeight;
+            }
+        },
+
+        createHorizontalSection(newMinX, newMinY, shipLength, board) {
+            this._setMinX(newMinX);
+            this._setMinY(newMinY);
+            this._setMaxX(shipLength, board);
+            this.setMaxY(3, board);
+        },
     };
+
+
 };
 
 export { boardSection };
